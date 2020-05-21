5380ad475d98feefb4e39baa5a75f0d67ef6da3e
Iwo Plaza <iwoplaza@gmail.com>
2020-05-21 23:20:54 +0200

    Updates

---
+++ precache-manifest.20818c21af1490d989cd126262d3c5be.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9cbae2685c12b1c334a3a03bf0173a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606807f3216f2483da4d",
+    "url": "/static/css/main.e771cd13.chunk.css"
+  },
+  {
+    "revision": "39c7ec8c6944b8cc4f9b",
+    "url": "/static/js/2.3b570ecc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b570ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606807f3216f2483da4d",
+    "url": "/static/js/main.d9cf5713.chunk.js"
+  },
+  {
+    "revision": "14a93580c2bf7f825c59",
+    "url": "/static/js/runtime-main.f97569eb.js"
+  },
+  {
+    "revision": "84b9229cdd32455f31d0c04934000552",
+    "url": "/static/media/logo_00.84b9229c.svg"
+  },
+  {
+    "revision": "abc68502dd1c231119391a61713faa77",
+    "url": "/static/media/logo_01.abc68502.svg"
+  },
+  {
+    "revision": "b16f2516c3c8cf6aa5d2c9d0f2afab99",
+    "url": "/static/media/logo_02.b16f2516.svg"
+  },
+  {
+    "revision": "bb533b1721913042bff531dfe341cac6",
+    "url": "/static/media/logo_03.bb533b17.svg"
+  },
+  {
+    "revision": "dec4b2f02391bbe41506c325cf94b939",
+    "url": "/static/media/logo_04.dec4b2f0.svg"
+  },
+  {
+    "revision": "a79fdca36e7538eae4cdd8f6df4b32c7",
+    "url": "/static/media/logo_05.a79fdca3.svg"
+  },
+  {
+    "revision": "e5ed06fb28d4934d0468e8beca7abfe7",
+    "url": "/static/media/logo_06.e5ed06fb.svg"
+  },
+  {
+    "revision": "61259a1f269e7e1eaabe80bb0e4f74d7",
+    "url": "/static/media/logo_07.61259a1f.svg"
+  },
+  {
+    "revision": "1f90a9efbf78df6d4e6daf69be2d056b",
+    "url": "/static/media/logo_08.1f90a9ef.svg"
+  },
+  {
+    "revision": "13f33cc2b620eaf00656d3e660fed8dc",
+    "url": "/static/media/logo_09.13f33cc2.svg"
+  },
+  {
+    "revision": "c1e831ec2a9f0d6e90051c394ca2cb9b",
+    "url": "/static/media/logo_10.c1e831ec.svg"
+  }
+]);
